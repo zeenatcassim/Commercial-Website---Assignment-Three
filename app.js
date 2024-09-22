8a1d220c3e20833d42dc1ac2cb7ee4c1fcc37e32
zeenatcassim <116756046+zeenatcassim@users.noreply.github.com>
2024-09-22 17:39:59 +0200

    API fetch and then and then

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
+//API
+
+const url = 'http://data.fixer.io/api/latest?access_key=803afba4fdd8cdacc37ab017860c99dd';
+const options = {
+    method: 'GET'
+};
+
+fetch(url, options)
+.then((response) => response.json())
+.then((data) => {
+ 
+    const rates = data.rates;
+    const dates = data.dates;
+    const base = data.base;
+
+    console.log(rates, dates, base);
+})
+.catch((error) => {
+    console.log("There is something wrong!", error);
+})
+
+/*
+async function fetchData(){
+try {
+const response = await fetch(url, options);
+const result = await response.text();
+console.log(result);
+//define data variables to use in graph?
+} catch(error){
+    console.log(error);
+}    
+}//fetchData();
+
+fetch(url, options).then(response => {
+if(!response.ok){
+    throw new Error("SOMETHING IS NOT RIGHT");
+}
+//console.log(response.json);
+
+})
+*/
+
+//setup svg
+
+
+
+
+//Navigation
+
+
+/*const essayBtn = document.getElementById("goToEssayBtn");
+if(essayBtn){
+essayBtn.addEventListener("click", function(){
+window.location.href ="essay.html";
+});    
+}
+*/
 
 //Sidebar Toggle//
 
