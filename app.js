783ffeffcdc4fd314e57de3542687c5f3812b011
zeenatcassim <116756046+zeenatcassim@users.noreply.github.com>
2024-09-24 17:09:08 +0200

    First data viz done

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
-//API
 
+//setup svg
+let WIDTH = 800, HEIGHT = 600, PADDING = 40;
+
+let svg = d3.select(".line-graph")
+.attr("width", WIDTH)
+.attr("height", HEIGHT);
+
+//domain - input data to visualize - rates 
+//range - space where I want to display the data - height and weight of graph
+//need minRate and maxRate values
+
+const yScale = d3.scaleLinear().range([HEIGHT, 0]);
+
+const xScale = d3.scaleLinear().range([0, WIDTH]);
+
+function appendLines(rates){
+   
+    svg.selectAll("path").remove();
+
+    //We want to update the data whenever the API is requested or data has been fetched
+    yScale.domain([d3.min(rates), d3.max(rates)]);
+    xScale.domain([0, rates.length - 1]);
+
+    console.log(`Y Scale Domain: ${yScale.domain()}`);
+    console.log(`X Scale Domain: ${xScale.domain()}`);
+
+    const line = d3.line().x((d,i) => xScale(i)).y(d => yScale(d));
+
+   svg.append("path")
+      .datum(rates)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2)
+      .attr("d", line);
+   
+}
+//API
+/*
 const url = 'http://data.fixer.io/api/latest?access_key=803afba4fdd8cdacc37ab017860c99dd';
 const options = {
     method: 'GET'
@@ -8,39 +45,21 @@ const options = {
 fetch(url, options)
 .then((response) => response.json())
 .then((data) => {
- 
-    const rates = data.rates;
-    const dates = data.dates;
-    const base = data.base;
+    const rates = Object.values(data.rates); 
+    console.log(rates);
 
-    console.log(rates, dates, base);
+   //call the function - create lines for graph
+    appendLines(rates);
+
+   // const rates = data.rates;
+   // const dates = data.dates;
+   // const base = data.base;
+
+   
 })
 .catch((error) => {
     console.log("There is something wrong!", error);
-})
-
-/*
-async function fetchData(){
-try {
-const response = await fetch(url, options);
-const result = await response.text();
-console.log(result);
-//define data variables to use in graph?
-} catch(error){
-    console.log(error);
-}    
-}//fetchData();
-
-fetch(url, options).then(response => {
-if(!response.ok){
-    throw new Error("SOMETHING IS NOT RIGHT");
-}
-//console.log(response.json);
-
-})
-*/
-
-//setup svg
+})*/
 
 
 
@@ -57,7 +76,7 @@ window.location.href ="essay.html";
 */
 
 //Sidebar Toggle//
-
+/*
 //references to buttons and sidebar
 const closeBtn = document.querySelector(".close-btn");
 const openBtn = document.querySelector(".open-btn");
@@ -70,4 +89,4 @@ closeBtn.addEventListener("click", function(){
 
 openBtn.addEventListener("click", function(){
     sideBar.classList.add("show-sidebar"); 
-})
+})*/
