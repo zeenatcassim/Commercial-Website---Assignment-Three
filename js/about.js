ed4b7744017eaeb3adc3c97dff210e10852b7228
zeenatcassim <116756046+zeenatcassim@users.noreply.github.com>
2024-10-06 18:31:12 +0200

    nav for home and about done

--- js/about.js
+++ js/about.js
@@ -1,4 +1,44 @@
+//Sidebar
+//references to buttons and sidebar
+const closeBtn = document.querySelector(".close-btn");
+const openBtn = document.querySelector(".open-btn");
+const sideBar = document.querySelector(".sidebar");
 
+//functions for the buttons
+closeBtn.addEventListener("click", function(){
+   sideBar.classList.remove("show-sidebar");
+})
+
+openBtn.addEventListener("click", function(){
+    sideBar.classList.add("show-sidebar"); 
+})
+
+//Navigation
+const homeBtn = document.querySelector(".home-btn");
+const designBtn = document.querySelector(".design-btn");
+const theoryBtn = document.querySelector(".theory-btn");
+const dataBtn = document.querySelector(".data-btn");
+
+if (designBtn) {
+    designBtn.addEventListener("click", function(){
+
+
+     window.location.href = "/Commercial-Website---Assignment-Three/html/design.html";
+      })
+} 
+ if (theoryBtn) {
+    theoryBtn.addEventListener("click", function(){
+    window.location.href = "/Commercial-Website---Assignment-Three/html/theory.html"; })
+}
+ if (dataBtn) {
+    dataBtn.addEventListener("click", function(){
+    window.location.href = "/Commercial-Website---Assignment-Three/html/data viz.html";
+    })
+}
+ if (homeBtn) {
+    homeBtn.addEventListener("click", function(){
+    window.location.href = "/Commercial-Website---Assignment-Three/index.html";})
+}
 
 //Displaying information about API dynamically through JS
  
@@ -21,4 +61,3 @@ fullSection.innerHTML = displayContents;
 }
 
 
- 
--- js/hm.js
+++ js/hm.js
@@ -1,13 +1,3 @@
-//Navigation
-
-
-/*const essayBtn = document.getElementById("goToEssayBtn");
-if(essayBtn){
-essayBtn.addEventListener("click", function(){
-window.location.href ="essay.html";
-});    
-}
-*/
 
 //Sidebar Toggle//
 
@@ -24,3 +14,29 @@ closeBtn.addEventListener("click", function(){
 openBtn.addEventListener("click", function(){
     sideBar.classList.add("show-sidebar"); 
 })
+
+//Navigation Buttons
+
+const designBtn = document.querySelector(".design-btn");
+const theoryBtn = document.querySelector(".theory-btn");
+const dataBtn = document.querySelector(".data-btn");
+const aboutBtn = document.querySelector(".about-btn");
+
+if (designBtn) {
+    designBtn.addEventListener("click", function(){
+     window.location.href = "html/design.html"; })
+} 
+ if (theoryBtn) {
+    theoryBtn.addEventListener("click", function(){
+    window.location.href = "html/theory.html"; })
+}
+ if (dataBtn) {
+    dataBtn.addEventListener("click", function(){
+    window.location.href = "html/data viz.html";
+    })
+}
+ if (aboutBtn) {
+    aboutBtn.addEventListener("click", function(){
+    window.location.href = "html/About.html";})
+}
+
